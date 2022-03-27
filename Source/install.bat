927a55fd043855c066b3795cf5964d9954ad5e0d
Lars Dehler <larsdehler95@yahoo.de>
2022-03-27 16:21:50 +0200

    add install.bat script

---
+++ Source/install.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set RIMWORLD_MODS_DIR=../../../Mods
+set MOD_NAME=CallATrader
+
+REM clear mod and copy latest files into the target mods dir
+rmdir /s /q "%RIMWORLD_MODS_DIR%/%MOD_NAME%"
+xcopy /e /i /y "../About" "%RIMWORLD_MODS_DIR%/%MOD_NAME%/About"
+xcopy /e /i /y "../Defs" "%RIMWORLD_MODS_DIR%/%MOD_NAME%/Defs"
+xcopy /e /i /y "../Assemblies" "%RIMWORLD_MODS_DIR%/%MOD_NAME%/Assemblies"
+xcopy /e /i /y "../Languages" "%RIMWORLD_MODS_DIR%/%MOD_NAME%/Languages"
+xcopy /e /i /y "../Textures" "%RIMWORLD_MODS_DIR%/%MOD_NAME%/Textures"
+
+REM start rimworld
+set STEAM_REGKEY=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam
+set STEAM_REGVAL=InstallPath
+
+REM Check for presence of key first.
+reg query %STEAM_REGKEY% /v %STEAM_REGVAL% 2>nul || (echo Steam install dir not found! & exit /b 1)
+
+REM query the value.
+REM pipe it through findstr in order to find the matching line that has the value. 
+REM only grab token 3 and the remainder of the line. %%b is what we are interested in here.
+set STEAM_INSTALL_DIR=
+for /f "tokens=2,*" %%a in ('reg query %STEAM_REGKEY% /v %STEAM_REGVAL% ^| findstr %STEAM_REGVAL%') do (
+    set STEAM_INSTALL_DIR=%%b
+)
+
+REM Possibly no value set
+if not defined STEAM_INSTALL_DIR (echo Steam.exe not found! & exit /b 1)
+
+REM start rimworld
+echo "%STEAM_INSTALL_DIR%\Steam.exe" -applaunch 294100
