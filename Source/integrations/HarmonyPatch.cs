243b6b0386437d7e05737f8604f75efafa9f2918
Lars Dehler <larsdehler95@yahoo.de>
2022-04-29 22:46:15 +0200

    move ICommunicalbe impl to separate class file

--- Source/integrations/HarmonyPatch.cs
+++ Source/integrations/HarmonyPatch.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Collections.Generic;
 using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using Verse;
-using Verse.AI;
 
 namespace Arakos.CallATrader
 {
@@ -13,7 +9,7 @@ namespace Arakos.CallATrader
     [HarmonyPatch(typeof(Building_CommsConsole), nameof(Building_CommsConsole.GetCommTargets))]
     public static class Building_CommsConsole_Patch
     {
-        private static readonly ICommunicable callTraderCommunicable = new CallATraderCommunicable();
+        private static readonly ICommunicable callTraderCommunicable = new ICommunicable_OrbitalTradersHub();
 
         // injects additional option into the ICommunicable list on the comms console
         [HarmonyPostfix]
@@ -23,61 +19,4 @@ namespace Arakos.CallATrader
         }
     }
 
-
-    public class CallATraderCommunicable : ICommunicable
-    {
-        private readonly FloatMenuOption callTraderOption = new FloatMenuOption("", null, Textures.ORBITAL_TRADER_HUB_ICON, Color.white, MenuOptionPriority.Default);
-
-        private Building_CommsConsole cachedConsole;
-
-        private Pawn cachedPawn;
-
-        // this method gets invoked permanently while the comms console options menu is open - hence use caching
-        public FloatMenuOption CommFloatMenuOption(Building_CommsConsole console, Pawn negotiator)
-        {
-
-            bool cacheValid = System.Object.Equals(cachedConsole, console) && System.Object.Equals(cachedPawn, negotiator);
-            cachedPawn = negotiator;
-            cachedConsole = console;
-
-            // get ticks the call trader action is disabled for
-            int disabledForTicks = CallATrader.state.traderRequestActionDisabledUntil - Find.TickManager.TicksAbs;
-
-            // must be done this way because the impl of disabled in FloatMenuOption is retarded
-            if (disabledForTicks > 0)
-            {
-                callTraderOption.Disabled = true;
-                callTraderOption.Label = (Constants.MOD_PREFIX + ".console.label.disabled").Translate(GenDate.ToStringTicksToPeriod(disabledForTicks, shortForm: false));
-            }
-            else if (callTraderOption.Disabled || !cacheValid)
-            {
-                callTraderOption.Disabled = false;
-                callTraderOption.Label = (Constants.MOD_PREFIX + ".console.label").Translate();
-                callTraderOption.action = () => negotiator.jobs.TryTakeOrderedJob(JobMaker.MakeJob(DefDatabase<JobDef>.GetNamed(Constants.JOB_DEF_NAME, true), console), JobTag.MiscWork);
-                FloatMenuUtility.DecoratePrioritizedTask(callTraderOption, negotiator, console);
-            }
-            return callTraderOption;
-        }
-        
-        public string GetCallLabel()
-        {
-            return "";
-        }
-
-        public Faction GetFaction()
-        {
-            Log.Message("getfactioncalled");
-            return null;
-        }
-
-        public string GetInfoText()
-        {
-            return "";
-        }
-
-        public void TryOpenComms(Pawn negotiator)
-        {
-            Log.Message("tryopencomms " + negotiator);
-        }
-    }
 }
---
+++ Source/rimworld/ICommunicable_OrbitalTradersHub.cs
@@ -0,0 +1,65 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Arakos.CallATrader
+{
+
+    public class ICommunicable_OrbitalTradersHub : ICommunicable
+    {
+        private readonly FloatMenuOption callTraderOption = new FloatMenuOption("", null, Textures.ORBITAL_TRADER_HUB_ICON, Color.white, MenuOptionPriority.Default);
+
+        private Building_CommsConsole cachedConsole;
+
+        private Pawn cachedPawn;
+
+        // this method gets invoked permanently while the comms console options menu is open - hence use caching
+        public FloatMenuOption CommFloatMenuOption(Building_CommsConsole console, Pawn negotiator)
+        {
+
+            bool cacheValid = System.Object.Equals(cachedConsole, console) && System.Object.Equals(cachedPawn, negotiator);
+            cachedPawn = negotiator;
+            cachedConsole = console;
+
+            // get ticks the call trader action is disabled for
+            int disabledForTicks = CallATrader.state.traderRequestActionDisabledUntil - Find.TickManager.TicksAbs;
+
+            // must be done this way because the impl of disabled in FloatMenuOption is retarded
+            if (disabledForTicks > 0)
+            {
+                callTraderOption.Disabled = true;
+                callTraderOption.Label = (Constants.MOD_PREFIX + ".console.label.disabled").Translate(GenDate.ToStringTicksToPeriod(disabledForTicks, shortForm: false));
+            }
+            else if (callTraderOption.Disabled || !cacheValid)
+            {
+                callTraderOption.Disabled = false;
+                callTraderOption.Label = (Constants.MOD_PREFIX + ".console.label").Translate();
+                callTraderOption.action = () => negotiator.jobs.TryTakeOrderedJob(JobMaker.MakeJob(DefDatabase<JobDef>.GetNamed(Constants.JOB_DEF_NAME, true), console), JobTag.MiscWork);
+                FloatMenuUtility.DecoratePrioritizedTask(callTraderOption, negotiator, console);
+            }
+            return callTraderOption;
+        }
+
+        public string GetCallLabel()
+        {
+            return "";
+        }
+
+        public Faction GetFaction()
+        {
+            Log.Message("getfactioncalled");
+            return null;
+        }
+
+        public string GetInfoText()
+        {
+            return "";
+        }
+
+        public void TryOpenComms(Pawn negotiator)
+        {
+            Log.Message("tryopencomms " + negotiator);
+        }
+    }
+}
